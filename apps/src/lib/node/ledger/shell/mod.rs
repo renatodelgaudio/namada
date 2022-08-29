cbb775bfe0f74409f52721cd541e86c507392006
brentstone <brentstone01@gmail.com>
2022-08-29 17:52:45 +0300

    apply inflation and distribute rewards

--- apps/src/lib/node/ledger/shell/mod.rs
+++ apps/src/lib/node/ledger/shell/mod.rs
@@ -855,6 +855,8 @@ mod test_utils {
                 },
                 byzantine_validators: vec![],
                 txs: vec![],
+                proposer_address: vec![],
+                votes: vec![],
             }
         }
     }
