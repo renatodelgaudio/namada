4d1d55d26006c447c3c6879cd75bac9cc9199977
James Hiew <james@chainfork.com>
2022-12-12 13:04:49 +0000

    Rename address -> owner

--- wasm/wasm_source/src/vp_user.rs
+++ wasm/wasm_source/src/vp_user.rs
@@ -24,12 +24,12 @@ enum KeyType<'a> {
 
 impl<'a> From<&'a storage::Key> for KeyType<'a> {
     fn from(key: &'a storage::Key) -> KeyType<'a> {
-        if let Some(address) = token::is_any_token_balance_key(key) {
-            Self::Token(address)
-        } else if let Some((_, address)) =
+        if let Some(owner) = token::is_any_token_balance_key(key) {
+            Self::Token(owner)
+        } else if let Some((_, owner)) =
             token::is_any_multitoken_balance_key(key)
         {
-            Self::Token(address)
+            Self::Token(owner)
         } else if proof_of_stake::is_pos_key(key) {
             Self::PoS
         } else if gov_storage::is_vote_key(key) {
