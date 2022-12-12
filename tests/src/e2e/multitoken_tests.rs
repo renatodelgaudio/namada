2dc562ec93193b513572a44babd2253a875c53d6
James Hiew <james@chainfork.com>
2022-12-12 18:12:09 +0000

    Set up multitoken balances in the test

--- tests/src/e2e/multitoken_tests.rs
+++ tests/src/e2e/multitoken_tests.rs
@@ -1,13 +1,22 @@
 use color_eyre::eyre::Result;
+use namada_core::types::address::Address;
+use namada_core::types::storage;
+use namada_tx_prelude::storage::KeySeg;
 
 use super::helpers::get_actor_rpc;
 use super::setup::constants::{
     wasm_abs_path, BERTHA, NAM, VP_ALWAYS_TRUE_WASM,
 };
 use super::setup::{self, Bin, Test, Who};
+use crate::e2e::setup::constants::{ALBERT, TX_WRITE_STORAGE_KEY_WASM};
 use crate::{run, run_as};
 
-fn get_address_for_alias(test: &Test, alias: &str) -> Result<String> {
+const MULTITOKEN_KEY_SEGMENT: &str = "tokens";
+const BALANCE_KEY_SEGMENT: &str = "balance";
+
+const ARBITRARY_SIGNER: &str = ALBERT;
+
+fn get_address_for_alias(test: &Test, alias: &str) -> Result<Address> {
     let wallet_find_address = run!(
         test,
         Bin::Wallet,
@@ -21,7 +30,7 @@ fn get_address_for_alias(test: &Test, alias: &str) -> Result<String> {
     let mut wallet_find_address = wallet_find_address.foreground();
 
     let (_, addr) = wallet_find_address.exp_regex(r"atest1\w+")?;
-    Ok(addr)
+    Ok(Address::decode(addr)?)
 }
 #[test]
 fn test_multitoken_transfer_implicit_to_implicit() -> Result<()> {
@@ -48,7 +57,7 @@ fn test_multitoken_transfer_implicit_to_implicit() -> Result<()> {
     let init_account_args = vec![
         "init-account",
         "--source",
-        BERTHA, // arbitrarily BERTHA
+        ARBITRARY_SIGNER,
         "--public-key",
         // Value obtained from
         // `namada::types::key::ed25519::tests::gen_keypair`
@@ -72,12 +81,55 @@ fn test_multitoken_transfer_implicit_to_implicit() -> Result<()> {
     client_init_account.exp_string("Transaction applied")?;
     client_init_account.assert_success();
 
+    // establish a multitoken VP with the following balances
+    // - #atest5blah/tokens/red/balance/$albert = 100
+    // - #atest5blah/tokens/red/balance/$bertha = 0
+
     let multitoken_vp_addr = get_address_for_alias(&test, multitoken_alias)?;
     println!("Fake multitoken VP established at {}", multitoken_vp_addr);
 
-    // establish a multitoken VP
-    // - #atest5blah/tokens/red/balance/$albert = 100
-    // - #atest5blah/tokens/red/balance/$bertha = 0
+    const RED_TOKEN_KEY_SEGMENT: &str = "red";
+
+    let albert_addr = get_address_for_alias(&test, ALBERT)?;
+    let albert_red_balance = storage::Key::from(multitoken_vp_addr.to_db_key())
+        .push(&MULTITOKEN_KEY_SEGMENT.to_owned())?
+        .push(&RED_TOKEN_KEY_SEGMENT.to_owned())?
+        .push(&BALANCE_KEY_SEGMENT.to_owned())?
+        .push(&albert_addr)?;
+    println!("Albert's red token balance key: {}", albert_red_balance);
+
+    let bertha_addr = get_address_for_alias(&test, BERTHA)?;
+    let bertha_red_balance = storage::Key::from(multitoken_vp_addr.to_db_key())
+        .push(&MULTITOKEN_KEY_SEGMENT.to_owned())?
+        .push(&RED_TOKEN_KEY_SEGMENT.to_owned())?
+        .push(&BALANCE_KEY_SEGMENT.to_owned())?
+        .push(&bertha_addr)?;
+    println!("Bertha's red token balance key: {}", bertha_red_balance);
+
+    let tx_code_path = wasm_abs_path(TX_WRITE_STORAGE_KEY_WASM);
+    let tx_data_path = test
+        .test_dir
+        .path()
+        .join("albert_red_token_balance_key.txt");
+    std::fs::write(&tx_data_path, format!("{albert_red_balance}"))?;
+
+    let tx_data_path = tx_data_path.to_string_lossy().to_string();
+    let tx_code_path = tx_code_path.to_string_lossy().to_string();
+    let tx_args = vec![
+        "tx",
+        "--signer",
+        ARBITRARY_SIGNER,
+        "--code-path",
+        &tx_code_path,
+        "--data-path",
+        &tx_data_path,
+        "--ledger-address",
+        &rpc_addr,
+    ];
+    let mut client_tx = run!(test, Bin::Client, tx_args, Some(40))?;
+    client_tx.exp_string("Transaction is valid.")?;
+    client_tx.exp_string("Transaction applied")?;
+    client_tx.assert_success();
 
     // make a transfer from Albert to Bertha, signed by Christel - this should
     // be rejected
