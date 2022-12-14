4bc0b0841dbdb2d6cf01485af7978588c4dc9b7a
James Hiew <james@chainfork.com>
2022-12-14 11:38:53 +0000

    Ensure all test wasms are covered

--- core/src/test_utils.rs
+++ core/src/test_utils.rs
@@ -10,16 +10,17 @@ use git2::Repository;
 #[allow(missing_docs)]
 #[derive(Debug, Clone, Copy)]
 pub enum TestWasms {
-    TxNoOp,
     TxMemoryLimit,
-    TxInitProposal,
     TxMintTokens,
-    TxReadStorageKey,
+    TxNoOp,
     TxProposalCode,
-    VpAlwaysTrue,
+    TxReadStorageKey,
+    TxWriteStorageKey,
     VpAlwaysFalse,
+    VpAlwaysTrue,
     VpEval,
     VpMemoryLimit,
+    VpReadStorageKey,
 }
 
 impl TestWasms {
@@ -27,16 +28,17 @@ impl TestWasms {
     /// able to.
     pub fn path(&self) -> PathBuf {
         let filename = match self {
-            TestWasms::TxNoOp => "tx_no_op.wasm",
             TestWasms::TxMemoryLimit => "tx_memory_limit.wasm",
-            TestWasms::TxInitProposal => "tx_init_proposal.wasm",
             TestWasms::TxMintTokens => "tx_mint_tokens.wasm",
-            TestWasms::TxReadStorageKey => "tx_read_storage_key.wasm",
+            TestWasms::TxNoOp => "tx_no_op.wasm",
             TestWasms::TxProposalCode => "tx_proposal_code.wasm",
-            TestWasms::VpAlwaysTrue => "vp_always_true.wasm",
+            TestWasms::TxReadStorageKey => "tx_read_storage_key.wasm",
+            TestWasms::TxWriteStorageKey => "tx_write_storage_key.wasm",
             TestWasms::VpAlwaysFalse => "vp_always_false.wasm",
+            TestWasms::VpAlwaysTrue => "vp_always_true.wasm",
             TestWasms::VpEval => "vp_eval.wasm",
             TestWasms::VpMemoryLimit => "vp_memory_limit.wasm",
+            TestWasms::VpReadStorageKey => "vp_read_storage_key.wasm",
         };
         let repo_root =
             Repository::discover(env::current_dir().unwrap()).unwrap();
