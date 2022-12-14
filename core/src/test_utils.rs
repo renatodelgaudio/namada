50a6d8f45e0fcff2a5f584f6662db85a9ad64ce2
James Hiew <james@chainfork.com>
2022-12-14 11:31:42 +0000

    Add VpAlwaysFalse to TestWasms enum

--- core/src/test_utils.rs
+++ core/src/test_utils.rs
@@ -17,6 +17,7 @@ pub enum TestWasms {
     TxReadStorageKey,
     TxProposalCode,
     VpAlwaysTrue,
+    VpAlwaysFalse,
     VpEval,
     VpMemoryLimit,
 }
@@ -33,6 +34,7 @@ impl TestWasms {
             TestWasms::TxReadStorageKey => "tx_read_storage_key.wasm",
             TestWasms::TxProposalCode => "tx_proposal_code.wasm",
             TestWasms::VpAlwaysTrue => "vp_always_true.wasm",
+            TestWasms::VpAlwaysFalse => "vp_always_false.wasm",
             TestWasms::VpEval => "vp_eval.wasm",
             TestWasms::VpMemoryLimit => "vp_memory_limit.wasm",
         };
